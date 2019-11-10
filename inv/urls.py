d629dfb17c749225d3443295d27ca48d00cf792e
Carlos <carlossanzgarcia81@gmail.com>
2019-11-10 17:36:44 +0100

    urls y html subcategoria

--- inv/urls.py
+++ inv/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import CategoriaView, CategoriaNew, CategoriaEdit, CategoriaDel
+from .views import CategoriaView, CategoriaNew, CategoriaEdit, CategoriaDel, \
+    SubCategoriaView
 
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('categorias/new', CategoriaNew.as_view(), name='categoria_new'),
     path('categorias/edit/<int:pk>/', CategoriaEdit.as_view(), name='categoria_edit'),
     path('categorias/delete/<int:pk>/', CategoriaDel.as_view(), name='categoria_del'),
+    path('subcategorias/', SubCategoriaView.as_view(), name='subcategoria_list'),
 ]
